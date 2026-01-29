1ee9b3779724b27022de85c9daacf4efbb5355cd
sunnix1818 <giacomocolatriano@gmail.com>
2026-01-29 09:42:17 +0100

    Update renderer.js

--- js/renderer.js
+++ js/renderer.js
@@ -1,19 +1,31 @@
 import { camera } from "./camera.js";
 
 export function drawMap(ctx, geo, nations){
-  ctx.setTransform(camera.zoom,0,0,camera.zoom,ctx.canvas.width/2,ctx.canvas.height/2);
-  ctx.clearRect(-5000,-5000,10000,10000);
+  ctx.setTransform(
+    camera.zoom,
+    0,
+    0,
+    camera.zoom,
+    ctx.canvas.width / 2 + camera.x,
+    ctx.canvas.height / 2 + camera.y
+  );
+
+  ctx.clearRect(-10000,-10000,20000,20000);
 
   geo.features.forEach(f=>{
-    const owner = f.properties.owner;
-    ctx.fillStyle = nations[owner]?.color || "#444";
+    const owner = f.properties.name;
+    ctx.fillStyle = nations[owner]?.color || "#555";
+    ctx.strokeStyle = "#111";
+    ctx.lineWidth = 1;
+
     ctx.beginPath();
-    f.geometry.coordinates[0].forEach(([x,y],i)=>{
-      i ? ctx.lineTo(x*10,-y*10) : ctx.moveTo(x*10,-y*10);
+    f.geometry.coordinates[0].forEach(([lon,lat],i)=>{
+      const x = lon * 8;
+      const y = -lat * 8;
+      i ? ctx.lineTo(x,y) : ctx.moveTo(x,y);
     });
     ctx.closePath();
     ctx.fill();
-    ctx.strokeStyle="#000";
     ctx.stroke();
   });
 }
