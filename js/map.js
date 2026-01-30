86e5cc7017f0f6a392afd71a710aee7977fc57e4
sunnix1818 <giacomocolatriano@gmail.com>
2026-01-30 09:49:32 +0100

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,16 +1,31 @@
-const canvas = document.getElementById("map");
-const ctx = canvas.getContext("2d");
-
-function resize(){
-canvas.width = innerWidth;
-canvas.height = innerHeight;
-}
-resize();
-onresize = resize;
+let camX=0, camY=0, zoom=1;
 
 function project(lon,lat){
 return [
-canvas.width/2 + lon*8,
-canvas.height/2 - lat*8
+canvas.width/2 + (lon*8+camX)*zoom,
+canvas.height/2 - (lat*8-camY)*zoom
 ];
 }
+
+let dragging=false, px,py;
+
+canvas.onmousedown=e=>{
+dragging=true; px=e.clientX; py=e.clientY;
+};
+
+onmouseup=()=>dragging=false;
+
+onmousemove=e=>{
+if(!dragging) return;
+camX += (e.clientX-px)/zoom;
+camY += (e.clientY-py)/zoom;
+px=e.clientX; py=e.clientY;
+drawWorld();
+drawArmies();
+};
+
+canvas.onwheel=e=>{
+zoom *= e.deltaY>0 ? 0.9 : 1.1;
+drawWorld();
+drawArmies();
+};
